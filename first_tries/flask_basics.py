ac1b05ec2fc7ab557387eab33531cbf90a0abaec
Jakub Rutrle <rutrle@seznam.cz>
2022-01-15 22:07:36 +0100

    templates and navbar

--- first_tries/flask_basics.py
+++ first_tries/flask_basics.py
@@ -1,16 +1,18 @@
 from doctest import debug
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # called routes or views usually
-    return "<h1>Hello, World!</h1>"
+@app.route('/home')
+def home_page():  # called routes or views usually
+    return render_template('home.html')
 
 
 @app.route('/about/<username>')  # dynamic address
 def about_page(username):
+    # bad, but functionall way to return html content
     return f"<h1>This is {username} page</h1>"
 
 
