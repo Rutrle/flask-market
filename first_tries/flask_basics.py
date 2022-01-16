7c736a5df42aa8f65dd642d64a63eb959a3fa366
Jakub Rutrle <rutrle@seznam.cz>
2022-01-16 14:27:46 +0100

    sending data to jinja template

--- first_tries/flask_basics.py
+++ first_tries/flask_basics.py
@@ -10,11 +10,17 @@ def home_page():  # called routes or views usually
     return render_template('home.html')
 
 
+@app.route('/market')
+def market_page():
+    return render_template('market.html', item_names='Phone')
+
+
+''' 
 @app.route('/about/<username>')  # dynamic address
 def about_page(username):
     # bad, but functionall way to return html content
     return f"<h1>This is {username} page</h1>"
-
+'''
 
 app.run(debug=True)
 # alternatively run it through console
