f22ca95ccb73176e40f8ed1460d5d0d50d84714f
Jakub Rutrle <rutrle@seznam.cz>
2022-01-23 09:16:20 +0100

    creating market directory, splitting the app into more files

---
+++ first_tries/routes.py
@@ -0,0 +1,18 @@
+@app.route('/')
+@app.route('/home')
+def home_page():  # called routes or views usually
+    return render_template('home.html')
+
+
+@app.route('/market')
+def market_page():
+    items = Item.query.all()
+    return render_template('market.html', items=items)
+
+
+''' 
+@app.route('/about/<username>')  # dynamic address
+def about_page(username):
+    # bad, but functionall way to return html content
+    return f"<h1>This is {username} page</h1>"
+'''
