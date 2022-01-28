4dc6f3dfe94c2f7104a95eaa25bee9f89c76fd25
Jakub Rutrle <rutrle@seznam.cz>
2022-01-28 10:17:28 +0100

    login view

--- FlaskMarket/market/__init__.py
+++ FlaskMarket/market/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = '1292023cdb7b8e39f924afab'
--- FlaskMarket/market/routes.py
+++ FlaskMarket/market/routes.py
@@ -1,7 +1,7 @@
 from market import app
 from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
-from market.forms import RegisterForm
+from market.forms import RegisterForm, LoginForm
 from market import db
 
 
@@ -36,6 +36,10 @@ def register_page():
 
     return render_template('register.html', form=form)
 
+@app.route('/login', methods=['GET','POST'])
+def login_page():
+    form = LoginForm()
+    return render_template('login.html', form=form)
 
 ''' 
 @app.route('/about/<username>')  # dynamic address
