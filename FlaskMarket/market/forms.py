cfea5ad8df2ff75ebf7c178ec4e294bf15b82b80
Jakub Rutrle <rutrle@seznam.cz>
2022-01-30 19:50:38 +0100

    item buying

--- FlaskMarket/market/forms.py
+++ FlaskMarket/market/forms.py
@@ -27,3 +27,8 @@ class LoginForm(FlaskForm):
     password = PasswordField(label='Password', validators=[DataRequired()])
     submit = SubmitField(label='Sign in')
 
+class PurchaseItemForm(FlaskForm):
+    submit=SubmitField(label='Purchase item!')
+
+class SellItemForm(FlaskForm):
+    submit=SubmitField(label='Sell item!')
